0bd70e843bfab356590abf6b59f7ac140aaeb47d
Njeru David <njeru789@gmail.com>
2023-02-15 10:49:52 +0300

    for loop oddeven

---
+++ Week-2/assignment.py
@@ -0,0 +1,1 @@
+#list all even, odd, prime numbers(from 1,100) separately
--- Week-2/for_loop.py
+++ Week-2/for_loop.py
@@ -1,2 +1,15 @@
 for i in range (0,7):
     print(i)
+#adding components in a for loop
+    sum =0
+for p in range (0,10):
+    sum = sum + p
+    print(sum)
+    product = 1
+for v in range (1,10):
+    product = product * v
+    print(product)  
+#factorial
+# 6! = 6*5*4*3*2*1
+6! = 6*5*4*3*2*1   
+print(6!)
