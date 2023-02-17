16bfcc87a0b477c979c68fea5f9a58dbe65e6db5
Njeru David <njeru789@gmail.com>
2023-02-17 08:44:32 +0300

    odd even

--- Week-2/oddeven.py
+++ Week-2/oddeven.py
@@ -1,6 +1,15 @@
-#odd numbers
+#odd numbers & even numbers
 for odd_numbers in range(1,100):
     if(odd_numbers%2 != 0):
       print(odd_numbers)
- for even_nu     
+print("--------------------------")     
+for even_numbers in range (1,100):
+    if(even_numbers%2 ==0):
+      print(even_numbers) 
     
+          #OR
+for i in range(1,100):
+  if(i%2 ==0):
+    print(i," is even ")
+  else:
+    print(i,"is odd")  
