2c76d56debf3fa5110f04741f6722ec81003a3c8
Njeru David <njeru789@gmail.com>
2023-02-17 10:53:42 +0300

    marathon lesson

---
+++ Week-2/strings.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+#This is a single line comment
+#python progrm to illustrate operators
+#Name : David Njeru
+#Email : njeru789@gmail.com
+#Date : 17/2/2023
+#File : strings.py
+poem = """This is a poem about nothing
+          It's funny people laugh about it
+"""
+print(poem)
+print(len(poem))
+f_name = "David"
+s_name = "Njeru"
+full_name = f_name + s_name
+age = 25
+
+print("Hello from Davi\n How are you\n I am fine")
+print("Hello from Davi/n How are you/n I am fine")
